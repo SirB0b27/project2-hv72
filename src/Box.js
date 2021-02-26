22199631dd2d6fd7c3d0eac266119d261827344e
SirB0b27 <61483017+SirB0b27@users.noreply.github.com>
2021-02-26 15:39:41 -0500

    Delete src directory

--- src/Box.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {useState, useRef} from 'react';
-export function Box(props){
-    return(
-        <div onClick={props.func} class="box">{props.val}</div>
-    )
-}
