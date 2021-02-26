06f7889759853412deaf4b27be894eafaf5fd67c
Hemanth Velan <hemanth2355@gmail.com>
2021-02-26 20:27:41 +0000

    Innitial Commit

---
+++ src/Box.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {useState, useRef} from 'react';
+export function Box(props){
+    return(
+        <div onClick={props.func} class="box">{props.val}</div>
+    )
+}
---
+++ src/HW9-App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { ListItem } from './ListItem.js'
+import { Board } from './Board.js'
+import './Board.css'
+
+function App() {
+  return (
+    <Board />
+  );
+}
+
+export default App;
