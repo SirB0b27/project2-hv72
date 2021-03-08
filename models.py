157187bef72ab7a9d249b8920edd630e10724995
Hemanth Velan <hemanth2355@gmail.com>
2021-03-08 22:57:46 +0000

    added database model pythong file

---
+++ models.py
@@ -0,0 +1,10 @@
+from app import db
+
+class Person(db.Model):
+    userName = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+    userScore = db.Column(db.Integer, nullable=False)
+    
+    
+    def __repr__(self):
+        return '<person %r>' % self.username
+        
